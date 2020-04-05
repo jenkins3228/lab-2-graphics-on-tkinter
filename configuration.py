01e39af89cf976c91c0d03eb7ae8af844703efc2
jenkins3228 <skreker99@gmail.com>
2020-04-05 23:58:09 +0300

    update

--- configuration.py
+++ configuration.py
@@ -1,4 +1,5 @@
 # from tkinter import *
+
 from tkinter import PhotoImage,Canvas,Tk,NW
 from random import randint
 import math
@@ -12,3 +13,35 @@ canvas.pack()
 
 canvas.create_rectangle(0,612,1080,720, fill="#484848",outline="#484848")#асфальт
 canvas.create_rectangle(0,655,1080,670, fill="white", outline="white")#линия на асфальте
+
+leftVertices = [
+    [70, 528],
+    [70, 603],
+    [151, 603],
+    [151, 528],
+]
+rightVertices = [
+    [289, 528],
+    [289, 603],
+    [368, 603],
+    [368, 528],
+]
+
+square = [
+    [281, 81],
+    [281, 162],
+    [375, 161],
+    [375, 81],
+]
+
+oval = [
+    [870, 16],
+    [1060, 184],
+]
+
+triangle = [
+    [132, 188],
+    [132, 270],
+    [220, 228],
+]
+
--- main.py
+++ main.py
@@ -1,77 +1,35 @@
 from Classes import *
-
-leftVertices = [
-    [70, 528],
-    [70, 603],
-    [151, 603],
-    [151, 528],
-]
-rightVertices = [
-    [289, 528],
-    [289, 603],
-    [368, 603],
-    [368, 528],
-]
+from configuration import *
 
 LeftrectangleCenter = (leftVertices[3][0]-((leftVertices[3][0]-leftVertices[0][0])/2),leftVertices[2][1]- ((leftVertices[2][1]-leftVertices[0][1])/2))
 RightrectangleCenter = (rightVertices[3][0]-((rightVertices[3][0]-rightVertices[0][0])/2),rightVertices[2][1]- ((rightVertices[2][1]-rightVertices[0][1])/2))
-
-new_square=leftVertices
-new_square1=rightVertices
-
 
 car=Car([29,407])
 
 leftRectangle=Rectangle(leftVertices)
 rightRectangle=Rectangle(rightVertices)
 
-square = [
-    [281, 81],
-    [281, 162],
-    [375, 161],
-    [375, 81],
-]
-
-oval = [
-    [870, 16],
-    [1060, 184],
-]
-
-triangle = [
-    [132, 188],
-    [132, 270],
-    [220, 228],
-]
-
-
 mirror=Mirror(triangle)
-mirror.startMirroring()
-# mirror.startMirroring()
-# mirror.startMirroring()
-
-# canvas.create_polygon(triangle,fill="red")
-
-# canvas.create_polygon(triangle1,fill="red")
 
 testSquare=Scaling(oval,1,"yellow")
-
-
 
 Speed=15
 i=0
 
 def main():
-    global new_square,new_square1,i
+    global leftVertices,rightVertices,i
     i+=Speed
     # testSquare.scalingSquare()
     testSquare.scalingOval()
+
     mirror.startMirroring()
-    new_square = leftRectangle.rotate(new_square,20, LeftrectangleCenter)
-    new_square1 = rightRectangle.rotate(new_square1,20, RightrectangleCenter)
+
+    leftVertices = leftRectangle.rotate(leftVertices,20, LeftrectangleCenter)
+    rightVertices = rightRectangle.rotate(rightVertices,20, RightrectangleCenter)
 
     car.Move(Speed)
-    leftRectangle.draw_rectangle(new_square,i)
-    rightRectangle.draw_rectangle(new_square1,i)
+    leftRectangle.draw_rectangle(leftVertices,i)
+    rightRectangle.draw_rectangle(rightVertices,i)
     root.after(100,main)
     
 main()
